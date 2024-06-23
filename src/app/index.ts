7b6518c0f6f607024ed884da6eacb3c009f534ee
upesh <upeshjeengar@gmail.com>
2024-06-23 11:20:09 +0530

    Day-2

---
+++ src/app/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { ApolloServer } from '@apollo/server';
+import {expressMiddleware} from '@apollo/server/express4';
+
+export async function initServer(){
+    const app=express();
+
+    app.use(bodyParser.json());
+
+    const graphqlServer=new ApolloServer({
+        typeDefs: `
+        type Query{
+            sayHello: String
+            sayHelloToMe(name:String!):String
+        }
+        `,
+        resolvers:{
+            Query:{
+                sayHello:()=>`Hello From the Melodi Team`,
+                sayHelloToMe:(parent:any,{name}:{name:string})=> 
+                    `Hey ${name}`, 
+            },
+        },
+    })
+
+    await graphqlServer.start();
+
+    app.use("/graphql",expressMiddleware(graphqlServer));
+    
+    return app;
+}
